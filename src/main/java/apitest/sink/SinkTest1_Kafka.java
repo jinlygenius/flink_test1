3385ab35af1f45d048e3f8f113fa543e055e3fdd
ella <jinlygenius@gmail.com>
2021-08-10 18:36:07 +0800

    sync

--- src/main/java/apitest/sink/SinkTest1_Kafka.java
+++ src/main/java/apitest/sink/SinkTest1_Kafka.java
@@ -5,7 +5,10 @@ import org.apache.flink.api.common.serialization.SimpleStringSchema;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.functions.sink.SinkFunction;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaProducer;
+
+import java.util.Properties;
 
 public class SinkTest1_Kafka {
     public static void main(String[] args) throws Exception{
@@ -15,13 +18,21 @@ public class SinkTest1_Kafka {
         // 读取数据
         DataStream<String> inputStream = env.readTextFile("/Users/ella/IdeaProjects/flink_test1/src/main/resources/sensor.txt");
 
+        // 从kafka读取数据
+//        Properties properties = new Properties();
+//        properties.setProperty("bootstrap.servers", "localhost:9092");
+//        DataStream<String> inputStream = env.addSource(
+//                new FlinkKafkaConsumer<String>("sensor", new SimpleStringSchema(), properties)
+//        );
+
         // 转成 SensorReading类型
         DataStream<String> dataStream = inputStream.map(line -> {
             String[] fields = line.split(",");
             return new SensorReading(fields[0], new Long(fields[1]), new Double(fields[2])).toString();
         });
 
-        dataStream.addSink(new FlinkKafkaProducer<String>("localhost:9092", "sink_test", new SimpleStringSchema()));
+        dataStream.addSink(new FlinkKafkaProducer<String>("123.56.4.42:9092", "test-kafka", new SimpleStringSchema()));
+//        dataStream.addSink(new FlinkKafkaProducer<String>("localhost:9092", "sensor", new SimpleStringSchema()));
         env.execute();
     }
 }
---
+++ src/main/java/apitest/sink/SinkTest2_Redis.java
@@ -0,0 +1,60 @@
+package apitest.sink;
+
+import apitest.beans.SensorReading;
+import org.apache.flink.api.common.serialization.SimpleStringSchema;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer;
+import org.apache.flink.streaming.connectors.redis.RedisSink;
+import org.apache.flink.streaming.connectors.redis.common.config.FlinkJedisPoolConfig;
+import org.apache.flink.streaming.connectors.redis.common.mapper.RedisCommand;
+import org.apache.flink.streaming.connectors.redis.common.mapper.RedisCommandDescription;
+import org.apache.flink.streaming.connectors.redis.common.mapper.RedisMapper;
+
+import java.util.Properties;
+
+public class SinkTest2_Redis {
+    public static void main(String[] args) throws Exception {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setParallelism(1);
+
+        // 读取数据
+        DataStream<String> inputStream = env.readTextFile("/Users/ella/IdeaProjects/flink_test1/src/main/resources/sensor.txt");
+
+        // 转成 SensorReading类型
+        DataStream<SensorReading> dataStream = inputStream.map(line -> {
+            String[] fields = line.split(",");
+            return new SensorReading(fields[0], new Long(fields[1]), new Double(fields[2]));
+        });
+
+        // 写入Redis。 定义jedis连接配置
+        FlinkJedisPoolConfig config = new FlinkJedisPoolConfig.Builder()
+                .setHost("localhost")
+                .setPort(6379)
+                .build();
+
+        dataStream.addSink(new RedisSink<>(config, new MyRedisMapper()));
+
+        env.execute();
+    }
+
+    // 自定义 redis mapper
+    public static class MyRedisMapper implements RedisMapper<SensorReading> {
+
+        // 定义保存数据到redis的命令，存成hash表： hset sensor_temp id:temperature
+        @Override
+        public RedisCommandDescription getCommandDescription() {
+            return new RedisCommandDescription(RedisCommand.HSET, "sensor_temp");
+        }
+
+        @Override
+        public String getKeyFromData(SensorReading sensorReading) {
+            return sensorReading.getId();
+        }
+
+        @Override
+        public String getValueFromData(SensorReading sensorReading) {
+            return sensorReading.getTemperature().toString();
+        }
+    }
+}
